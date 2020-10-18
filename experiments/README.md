1ec985ce3d506c5c17b0124400b188d947e89539
Young Mo Kang <ykang@salesforce.com>
2020-10-17 23:53:20 -0700

    add orcas data prep

--- experiments/README.md
+++ experiments/README.md
@@ -6,7 +6,20 @@ Once the train and test query files are prepared, refer to `Quick Start` to run 
 
 ## AOL Dataset
 
-Clone https://github.com/clovaai/subword-qac (commit df7a9a5) and follow it up to `Prepare Data` section
+Clone https://github.com/clovaai/subword-qac (commit df7a9a5) and perform `Prepare Data` section
 using the default setting. That is, download the data and split it into train, valid, and test sets.
 When the split script is complete, use `train.query.txt` and `test.query.txt` files 
 from `data/aol/full/` directory as the train and test sets.
+
+## ORCAS Dataset
+
+```bash script
+# download & unzip the query dataset
+wget https://msmarco.blob.core.windows.net/msmarcoranking/orcas.tsv.gz
+gunzip orcas.tsv.gz
+
+# extract queries and split into train, valid, and test
+cut -f2 -d$'\t' orcas.tsv | python ../script/split.py --output_prefix orcas
+```
+
+Use `orcas_train.txt` and `orcas_test.txt` files as the train and test files.
---
+++ script/split.py
@@ -0,0 +1,59 @@
+'''
+/*
+* Copyright (c) 2018, salesforce.com, inc.
+* All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause
+* For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+*/
+'''
+
+import sys
+import argparse
+import random
+import math
+
+"""
+Split a file into train, valid, and test
+Ignores empty lines
+"""
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--valid', type=float, default=0.1)
+    parser.add_argument('--test', type=float, default=0.1)
+    parser.add_argument('--output_prefix', type=str, default='')
+
+    args = parser.parse_args()
+    assert 0 <= args.valid < 1
+    assert 0 <= args.test < 1
+    assert args.valid + args.test < 1
+
+    train_file = args.output_prefix + '_train.txt'
+    valid_file = args.output_prefix + '_val.txt'
+    test_file = args.output_prefix + '_test.txt'
+
+    random.seed(args.seed)
+
+    queries = []
+    with sys.stdin as f:
+        for line in f:
+            query = line.rstrip()
+            if len(query) == 0: continue
+            queries.append(query)
+
+    random.shuffle(queries)
+    n_valid = math.floor(len(queries) * args.valid)
+    n_test = math.floor(len(queries) * args.test)
+    n_train = len(queries) - n_valid - n_test
+
+    with open(train_file, 'w', encoding='utf8') as f:
+        f.write('\n'.join(queries[:n_train]))
+    with open(valid_file, 'w', encoding='utf8') as f:
+        f.write('\n'.join(queries[n_train:n_train+n_valid]))
+    with open(test_file, 'w', encoding='utf8') as f:
+        f.write('\n'.join(queries[-n_test:]))
+
+
+if __name__ == '__main__':
+    main()
